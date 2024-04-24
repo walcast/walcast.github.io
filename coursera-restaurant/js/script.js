434e006afdca888f608580b59513c413087b4dfa
Walter Castillo Mendoza <walter.castillo@encora.com>
2024-04-23 19:22:50 -0600

    Remove unused function

--- coursera-restaurant/js/script.js
+++ coursera-restaurant/js/script.js
@@ -23,20 +23,6 @@ $(function () {
         insertHtml(selector, html);
     };
 
-    var switchMenuToActive = function () {
-        // Remove 'active' from home button
-        var classes = document.querySelector("#navHomeButton").className;
-        classes = classes.replace(new RegExp("active", "g"), "");
-        document.querySelector("#navHomeButton").className = classes;
-
-        // Add 'active' to menu button if not already there
-        classes = document.querySelector("#navMenuButton").className;
-        if (classes.indexOf("active") === -1) {
-            classes += " active";
-            document.querySelector("#navMenuButton").className = classes;
-        }
-    };
-
     $(document).ready(function() {
         showLoading('#main-content');
         $ajaxUtils.sendGetRequest(
